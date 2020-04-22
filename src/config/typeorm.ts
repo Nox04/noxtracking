d7f9124be786d4eb33fce027d76ea8d6d2118685
Nox <juan.angarita.11@gmail.com>
2020-04-22 14:06:44 -0500

    Working setup

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'db',
+  host: process.env.NODE_ENV === 'development' ? 'localhost' : 'db',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
