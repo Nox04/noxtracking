c71ec2ab7a62ba8b27a8b12cca2c59f04aa0ec01
Nox <juan.angarita.11@gmail.com>
2020-04-22 13:03:13 -0500

    Prod

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({
+      origin: '*.noxtracking.com/*',
+    });
+  }
+
   await app.listen(3000);
 }
 bootstrap();
